2df1e5637718c81d0ca064676368c2382f5be354
Damar <dtlagl77@gmail.com>
2024-12-13 23:19:07 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -24,20 +24,13 @@ Route::get('/login', function () {
 })->name('login');
 
 Route::get('/admin/products', [ProductController::class, 'index']);
- 
-
-
 
 // Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::get('admin/products/{id}', [ProductController::class, 'edit']);
 
-
-//rute ini error
-// Route::get('/admin/products/tambah', function () {
-//     return view('admin.tambah');
-// })->name('product.product');
+ 
 
 Route::get('/admin/tambah', function () {
     return view('admin.tambah');
@@ -46,14 +39,6 @@ Route::get('/admin/tambah', function () {
 Route::get('/admin/broadcast', function () {
     return view('admin.broadcast');
 })->name('admin.broadcast');
-
-// Route::get('admin/products/tambah', [ProductController::class, 'tambah'])->name('tambah.product');;
-// Route::get('/admin/edit', [ProductController::class, 'edit'])->name('admin.edit');
-
-
-// Route::get('/user/home', [ProductController::class, 'edit'])->name('admin.edit');
-
-
 
 Route::get('/otp/request', function () {
     return view('requestOtp');
@@ -67,34 +52,7 @@ Route::get('/register', function () {
 Route::post('/otp/request', [OtpController::class, 'requestOTP'])->name('otp.request');
 Route::get('/otp/verify', [OtpController::class, 'otpVerify'])->name('otp.verify');
 Route::post('/otp/verify', [OtpController::class, 'postVerify'])->name('post.verify');
-// Route::get('/register', [OtpController::class, 'getRegister'])->name('register');
-
-
-
-
-//Ga dipakai
-// Route::prefix('admin')->group(function () {
-//     Route::get('broadcast', function () {
-//         return view('admin.broadcast');
-//     })->name('admin.broadcast');
-
-//     Route::get('menu', function () {
-//         return view('admin.menu');
-//     })->name('admin.menu');
-
-//     Route::get('tambah-menu', function () {
-//         return view('admin.tambah');
-//     })->name('admin.tambah.menu');
-
-//     Route::get('edit-menu', function () {
-//         return view('admin.edit');
-//     })->name('admin.edit.menu');
-
-//     Route::get('cek', function () {
-//         return view('admin.cek');  // Admin page
-//     })->name('admin.cek');
-// });
-
+ 
 
 
 Route::prefix('user')->group(function () {
